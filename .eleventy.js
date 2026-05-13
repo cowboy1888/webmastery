f1e6607aa93a2d1301fbfe817d6fd2cc1a5e7fa6
cowboy1888 <canadianmoonie@gmail.com>
2026-05-12 22:57:44 -0400

    fix: removed blogposts/ from .gitignore so 11ty allows blog links to appear on blog.njk

--- .eleventy.js
+++ .eleventy.js
@@ -1,26 +1,13 @@
 module.exports = function(eleventyConfig) {
   
-  // 1. Passthrough File Copy
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("assets");
-
-  // 2. Watch Targets
   eleventyConfig.addWatchTarget("./css/");
 
-  // 3. Add a collection for blog posts
-  eleventyConfig.addCollection("blogPosts", function(collectionApi) {
-    // NOTE: Make sure to change this path to match your actual folder structure!
-    return collectionApi.getFilteredByGlob("path/to/your/blog/posts/*.{html,md}").sort((a, b) => {
-      return b.date - a.date; // Sort in reverse chronological order (newest first)
-    });
-  });
-
-  // 4. Add date filter for RSS
   eleventyConfig.addFilter("dateToRfc822", function(date) {
     return new Date(date).toUTCString();
   });
 
-  // 5. Return Object (MUST BE AT THE VERY BOTTOM)
   return {
     dir: {
       input: ".",        
@@ -28,5 +15,4 @@ module.exports = function(eleventyConfig) {
       output: "_site"    
     }
   };
-  
 };
